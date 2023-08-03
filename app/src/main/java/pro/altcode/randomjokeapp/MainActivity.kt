d873114f3d67c6a95b608c70878e633660e7e7e6
Blazhievskii Artem <ablazhievskii@gmail.com>
2023-08-04 00:25:11 +0800

    Initial commit

---
+++ app/src/main/java/pro/altcode/randomjokeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package pro.altcode.randomjokeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
